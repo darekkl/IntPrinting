3b330a76dee51bcf1a1a67ee06bcbd071b199932
darekkl <36088489+darekkl@users.noreply.github.com>
2019-01-26 15:55:32 +0100

    Initial commit

---
+++ int_printing.h
@@ -0,0 +1,13 @@
+inline void put_char(char c) {
+    putchar(c);
+}
+
+inline char get_char(void) {
+    return getchar();
+}
+
+void put_string(char *s);
+long long get_int(void);
+unsigned long long get_unsigned_int(void);
+void put_int(long long x);
+void put_unsigned_int(unsigned long long x);
